bc44a7f59c624ca266730a0efd39dda4596d049e
rana-raafat <ranaraafat7801@gmail.com>
2023-03-13 23:40:02 +0200

    initial http rest api class

--- app/src/main/java/com/wikitude/samples/MainActivity.java
+++ app/src/main/java/com/wikitude/samples/MainActivity.java
@@ -23,6 +23,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     private Button button1;
     private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, HandDetectionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RestAPI.class);
                 startActivity(intent);
             }
         });
